a2da5a8058527668ce27a862a41562f90b82f9a9
Camille Dejoye <cdejoye@houra.fr>
2018-10-24 21:23:21 +0200

    Adds a deployment script

---
+++ bin/.deploy_scripts/deploy_nvim.sh
@@ -0,0 +1,63 @@
+# I'm not sure my NeoVim configuration is sill working with vim 8 anymore
+# I'll assume not and not even bother installing it for now
+
+if [ -z $DOTFILES ]; then
+    echo "This script is not meant to be used directly"
+    exit
+fi
+
+NVIMDIR="${XDG_CONFIG_HOME:-$HOME/.config}/nvim"
+NVIMRC="$NVIMDIR/init.vim"
+
+check_for_nvim_repository() {
+    echo "Checking to see if neovim repository is in you system"
+
+    if ! grep -rq neovim /etc/apt/; then
+        answer=$( ask_for_yes_or_no "The repository for neovim is not on your system, add it ?" )
+
+        if echo "answer" | grep -iq "^y"; then
+            sudo apt-add-repository ppa:neovim-ppa/stable
+            sudo apt-get update
+        fi
+    else
+        echo "The repository is already in your system"
+    fi
+}
+
+prompt_install_python_module() {
+    answer=$( ask_for_yes_or_no "$1 module is not installed. Would you like to install it?" )
+
+    if echo "$answer" | grep -iq "^y" ;then
+        sudo pip${2:-} install --upgrade $1
+    fi
+}
+
+check_for_python_module() {
+    echo "Checking to see if the python module $1 is installed"
+
+    if ! pip${2:-} show -qqq "$1"; then
+        prompt_install_python_module $1 ${2:-}
+    else
+        echo "$1 is installed."
+    fi
+}
+
+check_for_nvim_repository
+echo
+
+check_for_software nvim neovim
+echo
+
+check_for_software pip "python-dev python-pip"
+echo
+
+check_for_software pip3 "python3-dev python3-pip"
+echo
+
+check_for_python_module neovim
+echo
+
+check_for_python_module neovim 3
+echo
+
+create_conf_file "source $DOTFILES/vim/vimrc" "$NVIMRC"
---
+++ bin/.deploy_scripts/lib.sh
@@ -0,0 +1,99 @@
+ask_for_yes_or_no() {
+    echo -n >&2 "$1 (y/n) "
+
+    old_stty_cfg=$(stty -g)
+    stty raw -echo
+    answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+    stty $old_stty_cfg && echo >&2
+
+    echo $answer
+}
+
+prompt_install() {
+    answer=$( ask_for_yes_or_no "$1 is not installed. Would you like to install it?" )
+
+    if echo "$answer" | grep -iq "^y" ;then
+        # This could def use community support
+        if [ -x "$(command -v apt-get)" ]; then
+            sudo apt-get install $1 -y
+
+        elif [ -x "$(command -v brew)" ]; then
+            brew install $1
+
+        elif [ -x "$(command -v pkg)" ]; then
+            sudo pkg install $1
+
+        elif [ -x "$(command -v pacman)" ]; then
+            sudo pacman -S $1
+
+        else
+            echo "I'm not sure what your package manager is! Please install $1 on your own and run this deploy script again. Tests for package managers are in the deploy script you just ran starting at line 13. Feel free to make a pull request at https://github.com/parth/dotfiles :)"
+        fi
+    fi
+}
+
+check_for_software() {
+    echo "Checking to see if $1 is installed"
+    if ! [ -x "$(command -v $1)" ]; then
+        prompt_install ${2:-$1}
+    else
+        echo "$1 is installed."
+    fi
+}
+
+check_default_shell() {
+    if [ -z "${SHELL##*zsh*}" ] ;then
+        echo "Default shell is zsh."
+    else
+        answer=$( ask_for_yes_or_no "Default shell is not zsh. Do you want to chsh -s \$(which zsh)?" )
+
+        if echo "$answer" | grep -iq "^y" ;then
+            chsh -s $(which zsh)
+        else
+            echo "Warning: Your configuration won't work properly. If you exec zsh, it'll exec tmux which will exec your default shell which isn't zsh."
+        fi
+    fi
+}
+
+backup_conf_file() {
+    if ! ls "$1" >/dev/null 2>&1; then
+        return 0
+    fi 
+
+    echo
+    answer=$( ask_for_yes_or_no "Would you like to backup $1 ?" )
+
+    if echo "$answer" | grep -iq "^y" ;then
+        mv -i "$1" "$1.previous-dotfiles-deployement"
+    else
+        echo "\nNot backing up $1."
+    fi
+}
+
+do_create_conf_file() {
+    if ! ls "$( dirname "$2" )" >/dev/null 2>&1; then
+        mkdir -p "$( dirname "$2" )"
+    fi
+
+    printf "$1" > "$2"
+
+    echo
+}
+
+create_conf_file() {
+    if ! ls "$2" >/dev/null 2>&1; then
+        do_create_conf_file "$1" "$2"
+
+        return 0
+    fi
+
+    backup_conf_file $2
+    echo
+
+    answer=$( ask_for_yes_or_no "The file '$2' already exists, do you want to overwrite it ?" )
+    if echo "$answer" | grep -iq "^y" ;then
+        do_create_conf_file "$1" "$2"
+    else
+        echo "Add the following line to '$2':\n$1\n"
+    fi
+}
