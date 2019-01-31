f6ebb2c996b3ce3c56c1e7bb743b67949696f23d
Camille Dejoye <cdejoye@houra.fr>
2019-01-31 19:43:15 +0100

    Adds new short aliases for day to day commands

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -19,3 +19,22 @@ alias dcd='dc down'
 alias sf='bin/console'
 
 alias prsync='rsync --delete -Cav --exclude=var/ --exclude=vendor/ --exclude=node_modules/'
+
+alias g='git'
+alias ga='git add'
+alias gc='git commit'
+alias gs='git status -sb'
+alias gp='git push'
+alias gr='git pull -r'
+alias gd='git diff -w'
+alias gds='git diff -w --staged'
+alias gl='git ll'
+alias gla='git lola'
+
+alias c='composer'
+alias ct='composer test'
+alias cs='composer cs'
+alias csf='composer cs-fix'
+alias composers='composer'
+
+alias most_used_commands="history | awk '{a[\$2]++}END{for(i in a){print a[i] \"s \" i}}' | sort -rn | head -20"
