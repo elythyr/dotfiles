bb421ba1b1bf88f32d8388610c5baee30f2434ae
Camille Dejoye <cdejoye@houra.fr>
2019-02-05 18:41:35 +0100

    Adds some more aliases to ease and speed up my workflow

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -19,22 +19,31 @@ alias dcd='dc down'
 alias sf='bin/console'
 
 alias prsync='rsync --delete -Cav --exclude=var/ --exclude=vendor/ --exclude=node_modules/'
+alias pgrep='grep --exclude-dir=vendor --exclude-dir=.git/ --exclude-dir=var/ --exclude-dir=node_modules/'
 
 alias g='git'
 alias ga='git add'
+alias gap='git add -p'
 alias gc='git commit'
 alias gs='git status -sb'
 alias gp='git push'
 alias gr='git pull -r'
+alias gri='git rebase -i'
+alias grb='git rebase -i $(git merge-base master HEAD)'
 alias gd='git diff -w'
 alias gds='git diff -w --staged'
 alias gl='git ll'
 alias gla='git lola'
+alias gss='git stash push' # Alias for the old git stash save, so that gsp is git stash pop
+alias gsp='git stash pop'
+alias gsl='git stash list'
+alias gck='git checkout'
 
 alias c='composer'
 alias ct='composer test'
 alias cs='composer cs'
 alias csf='composer cs-fix'
+alias cc='composer check'
 alias composers='composer'
 
 alias most_used_commands="history | awk '{a[\$2]++}END{for(i in a){print a[i] \"s \" i}}' | sort -rn | head -20"
