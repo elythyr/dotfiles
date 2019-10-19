4c647a3b99bb3085b7d8324e12a5a11bfaf73c25
Camille Dejoye <camilledejoye@gmail.com>
2019-10-19 14:50:21 +0200

    Adds a script to install bat

---
+++ bin/.deploy_scripts/deploy_bat.sh
@@ -0,0 +1,33 @@
+if ! type 'prompt_install' >/dev/null 2>&1; then
+    . ./lib.sh
+fi
+
+install_bat() {
+    echo "Fetching last version of bat..."
+
+    BAT_URL=`curl -s https://api.github.com/repos/sharkdp/bat/releases/latest | grep 'browser_download_url.*bat_.*_amd64.deb' | cut -d: -f2,3 | tr -d \"\ `
+    BAT_BIN="/tmp/$(basename "$BAT_URL")"
+
+    echo "Downloading $(basename $BAT_BIN):"
+    wget -q --show-progress --progress=bar:force -P "$(dirname $BAT_BIN)" -q "$BAT_URL"
+
+    echo "Installing bat:"
+    sudo dpkg -i "$BAT_BIN"
+
+    echo "bat is now installed."
+    rm -fr "$BAT_BIN"
+}
+
+check_install_bat() {
+    if ! [ -x "$(command -v bat)" ]; then
+        answer=$( ask_for_yes_or_no "bat is not installed, would you like to install it?" )
+
+        if echo "$answer" | grep -iq "^y" ;then
+            install_bat
+        fi
+    else
+        echo "bat is already installed, nothing to do."
+    fi
+}
+
+check_install_bat
