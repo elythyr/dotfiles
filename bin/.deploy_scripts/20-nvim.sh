f9c142339afd8eaeb40288c5de5d5cc3e91501bf
Camille Dejoye <camille.dejoye@wynd.eu>
2020-02-14 17:14:40 +0100

    Update neovim deployment for python modules

--- bin/.deploy_scripts/20-nvim.sh
+++ bin/.deploy_scripts/20-nvim.sh
@@ -34,7 +34,7 @@ prompt_install_python_module() {
     answer=$( ask_for_yes_or_no "The module is not installed, would you like to install it?" )
 
     if echo "$answer" | grep -iq "^y" ;then
-        sudo -H pip${2:-} install --upgrade $1
+        python${2:-} -m pip install --user --upgrade pynvim
 
         echo "The module was properly installed."
     fi
@@ -65,7 +65,7 @@ title "PIP3" 2
 check_for_software pip3 "python3-dev python3-pip"
 
 title "Python module" 2
-check_for_python_module neovim
+check_for_python_module neovim 2
 
 title "Python3 module" 2
 check_for_python_module neovim 3
