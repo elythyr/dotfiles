11484791972c2bfa9350f94156154b371fc9a9a5
Camille Dejoye <camilledejoye@gmail.com>
2020-03-14 15:24:37 +0100

    Allow specific zsh config per directory

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -5,3 +5,14 @@
 #     ls;
 # }
 # alias cd='cdls'
+
+# Load a local config when switching directory
+# The goal is to be able to override aliases in some specific cases
+function chpwd() {
+    # Always reload the main file to erase any previous override
+    source $HOME/.zshrc
+
+    if [ -r $PWD/.zsh_config ]; then
+        source $PWD/.zsh_config
+    fi
+}
