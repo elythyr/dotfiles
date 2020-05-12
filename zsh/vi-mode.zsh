29c962203a793e1a4628b3e2a713e72a0ae7f2eb
Camille Dejoye <camilledejoye@gmail.com>
2020-05-12 16:42:09 +0200

    zsh: double escape enter normal mode instantly

--- zsh/vi-mode.zsh
+++ zsh/vi-mode.zsh
@@ -1,3 +1,7 @@
 # Smart history, use the begining of the line something is already typed
-vim-mode-bindkey viins vicmd -- up-line-or-beginning-search             '^P'
-vim-mode-bindkey viins vicmd -- down-line-or-beginning-search           '^N'
+vim-mode-bindkey viins vicmd -- up-line-or-beginning-search '^P'
+vim-mode-bindkey viins vicmd -- down-line-or-beginning-search '^N'
+
+# Double escape put into normal mode without delay
+# But it overrides Alt + arrow_keys, which I don't care
+bindkey -rpM viins '^[^['
