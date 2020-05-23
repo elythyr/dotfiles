5247480bb2c59f30ab964e1ee964e90ff2848076
Camille Dejoye <camilledejoye@gmail.com>
2020-05-23 17:54:38 +0200

    shell: reorganize shell files like .profile
    I took inspiration from https://blog.flowblok.id.au/2013-02/shell-startup-scripts.html
    
    The `shell` directory will contains everything needed for all shells:
    - aliases
    - environement variables
    - etc
    
    The `zsh` directory will contains specific configuration for ZSH while
    sourcings the corresponding files from `shell`.
    For instance `zsh/interactive` is the file loaded whenever an
    interactive shell is started: it will configure oh-my-zsh and then
    source `shell/interactive` which will define aliases and stuff.

--- zsh/oh-my-zsh/fzf.zsh
+++ zsh/oh-my-zsh/fzf.zsh
@@ -4,21 +4,19 @@ function source_file_if_exist() {
     [ -f "$1" ] && source $1
 }
 
-fzf_path=$([ -d "/usr/share/fzf" ] && echo "/usr/share/fzf" || echo "$HOME/.fzf/shell")
-
 fzf_path="/usr/share/fzf"
 if [ -d $fzf_path ]; then
-elif [ -d "$HOME/.fzf/shell" ]; then
-    fzf_path="$HOME/.fzf/shell"
+elif [ -d "$DOTFILES/fzf/shell" ]; then
+    fzf_path="$DOTFILES/fzf/shell"
     # Avoid duplication when re-sourcing
     typeset -U PATH
-    export PATH="$PATH:$HOME/.fzf/bin"
+    export PATH="$PATH:$DOTFILES/fzf/bin"
 fi
 
 source_file_if_exist "$fzf_path/completion.zsh"
 source_file_if_exist "$fzf_path/key-bindings.zsh"
-source_file_if_exist "$HOME/.fzf-git/functions.sh"
-source_file_if_exist "$HOME/.fzf-git/key-binding.zsh"
+source_file_if_exist "$DOTFILES/fzf-git/functions.sh"
+source_file_if_exist "$DOTFILES/fzf-git/key-binding.zsh"
 
 unset fzf_path
 unset -f source_file_if_exist
